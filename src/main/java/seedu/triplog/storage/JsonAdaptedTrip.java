14dd32aaca5be5b598522c37d3f1f1a545410bed
poise3 <105375844+poise3@users.noreply.github.com>
2026-03-08 21:05:52 +0800

    Fix import order by group

--- src/main/java/seedu/triplog/storage/JsonAdaptedTrip.java
+++ src/main/java/seedu/triplog/storage/JsonAdaptedTrip.java
@@ -1,7 +1,7 @@
 package seedu.triplog.storage;
 
-import java.util.ArrayList;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
