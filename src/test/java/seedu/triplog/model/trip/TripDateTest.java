a6b0dbfbef731a53e8094c794644f3cab853b139
Jin Hui <105375844+poise3@users.noreply.github.com>
2026-03-23 16:07:51 +0800

    Fix import order

--- src/test/java/seedu/triplog/model/trip/TripDateTest.java
+++ src/test/java/seedu/triplog/model/trip/TripDateTest.java
@@ -1,6 +1,9 @@
 package seedu.triplog.model.trip;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.triplog.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
