4edf1f62a4ae3b3028740ba4eead2bee66ea85d9
Derrick Low <lowderrickym@gmail.com>
2026-03-25 21:53:58 +0800

    add space between imports

--- src/test/java/seedu/triplog/ui/HelpWindowTest.java
+++ src/test/java/seedu/triplog/ui/HelpWindowTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
