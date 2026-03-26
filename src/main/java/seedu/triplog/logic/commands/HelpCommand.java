67fd9f890526b04898513b5092e645024f62e8ab
Derrick Low <lowderrickym@gmail.com>
2026-03-26 14:43:50 +0800

    edit switch statements

--- src/main/java/seedu/triplog/logic/commands/HelpCommand.java
+++ src/main/java/seedu/triplog/logic/commands/HelpCommand.java
@@ -1,5 +1,6 @@
 package seedu.triplog.logic.commands;
 
+import seedu.triplog.commons.core.CommandUsage;
 import seedu.triplog.logic.commands.exceptions.CommandException;
 import seedu.triplog.model.Model;
 
@@ -18,55 +19,6 @@ public class HelpCommand extends Command {
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
     public static final String MESSAGE_UNKNOWN_HELP_COMMAND =
             "Unknown command: '%s'. Type 'help' to open the full command guide.";
-
-    public static final String ADD_USAGE =
-            "add n/<destination> [p/<phone>] [e/<email>] [a/<address>] "
-                    + "[sd/<start-date>] [ed/<end-date>] [t/<tag>]...\n"
-                    + "  Records a new trip. Items in [square brackets] are optional. Dates must be YYYY-MM-DD.\n"
-                    + "  e.g. add n/Tokyo sd/2026-03-01 t/food";
-
-    public static final String EDIT_USAGE =
-            "edit INDEX [n/<destination>] [p/<phone>] [e/<email>] [a/<address>] "
-                    + "[sd/<start-date>] [ed/<end-date>] [t/<tag>]...\n"
-                    + "  Edits the trip at the specified index. At least one field must be provided.\n"
-                    + "  e.g. edit 1 n/Paris sd/2026-05-01";
-
-    public static final String DELETE_USAGE =
-            "delete <INDEX> | <START-END> | <PREFIX/VALUE> | sd/<start-date> ed/<end-date>\n"
-                    + "  Removes trip(s) from the currently displayed list.\n"
-                    + "  All delete operations first show a preview before confirmation.\n"
-                    + "  Press Enter again to confirm deletion.\n"
-                    + "  INDEX must be a positive integer (1, 2, 3, …).\n"
-                    + "  START must be <= END for range deletion.\n"
-                    + "  Only one PREFIX can be used for field deletion.\n"
-                    + "  Dates must be YYYY-MM-DD.\n"
-                    + "  e.g.  delete 2\n"
-                    + "        delete 1-3\n"
-                    + "        delete n/Tokyo\n"
-                    + "        delete t/family\n"
-                    + "        delete sd/2026-03-01 ed/2026-05-10";
-
-    public static final String TAG_USAGE =
-            "tag <INDEX> <tag-name>\n"
-                    + "  Adds a keyword tag to an existing trip.\n"
-                    + "  tag-name must be alphanumeric and may contain spaces.\n"
-                    + "  e.g.  tag 1 adventure    or    tag 1 night market";
-
-    public static final String FIND_USAGE =
-            "find <KEYWORD> [MORE_KEYWORDS]...\n"
-                    + "  Finds trips whose names contain any of the given keywords.\n"
-                    + "  e.g. find Tokyo Osaka";
-
-    public static final String FILTER_USAGE =
-            "filter sd/<start-date> ed/<end-date>\n"
-                    + "  Filters trips occurring within the specified date range.\n"
-                    + "  e.g. filter sd/2026-01-01 ed/2026-03-31";
-
-    public static final String LIST_USAGE =
-            "list [sort/KEY]\n"
-                    + "  Displays all trips with a summary (Upcoming, Ongoing, Completed, Planning).\n"
-                    + "  Optional sort keys: name, start, end, len.\n"
-                    + "  e.g. list, list sort/name, list sort/len";
 
     private final String argument;
 
@@ -91,19 +43,19 @@ public class HelpCommand extends Command {
         case COMMAND_WORD:
             return MESSAGE_USAGE;
         case AddCommand.COMMAND_WORD:
-            return ADD_USAGE;
+            return CommandUsage.ADD_USAGE;
         case EditCommand.COMMAND_WORD:
-            return EDIT_USAGE;
+            return CommandUsage.EDIT_USAGE;
         case DeleteCommand.COMMAND_WORD:
-            return DELETE_USAGE;
+            return CommandUsage.DELETE_USAGE;
         case TagCommand.COMMAND_WORD:
-            return TAG_USAGE;
+            return CommandUsage.TAG_USAGE;
         case FindCommand.COMMAND_WORD:
-            return FIND_USAGE;
+            return CommandUsage.FIND_USAGE;
         case FilterCommand.COMMAND_WORD:
-            return FILTER_USAGE;
+            return CommandUsage.FILTER_USAGE;
         case ListCommand.COMMAND_WORD:
-            return LIST_USAGE;
+            return CommandUsage.LIST_USAGE;
         default:
             throw new CommandException(String.format(MESSAGE_UNKNOWN_HELP_COMMAND, commandWord));
         }
