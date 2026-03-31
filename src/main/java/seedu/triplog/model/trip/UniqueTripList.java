24b7a0aed073d2a6f1600915921ca367ca1ab903
poise3 <105375844+poise3@users.noreply.github.com>
2026-03-31 17:06:26 +0800

    Extract helper function

--- src/main/java/seedu/triplog/model/trip/UniqueTripList.java
+++ src/main/java/seedu/triplog/model/trip/UniqueTripList.java
@@ -61,11 +61,7 @@ public class UniqueTripList implements Iterable<Trip> {
             throw new TripNotFoundException();
         }
 
-        boolean isDuplicate = internalList.stream()
-                .filter(trip -> !trip.equals(target))
-                .anyMatch(trip -> trip.isSameTrip(editedTrip));
-
-        if (isDuplicate) {
+        if (containsExcluding(editedTrip, target)) {
             throw new DuplicateTripException();
         }
 
@@ -151,5 +147,14 @@ public class UniqueTripList implements Iterable<Trip> {
             }
         }
         return true;
+    }
+
+    /**
+     * Returns true if {@code toCheck} contains excluded trip.
+     */
+    private boolean containsExcluding(Trip toCheck, Trip excluded) {
+        return internalList.stream()
+                .filter(trip -> !trip.equals(excluded))
+                .anyMatch(trip -> trip.isSameTrip(toCheck));
     }
 }
