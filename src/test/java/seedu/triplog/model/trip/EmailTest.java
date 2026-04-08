94d8ed33880fffd93b2791fb8ee6b52059410643
ths1959 <ths2891503@gmail.com>
2026-04-09 00:39:56 +0800

    Fix EmailTest to allow 1-char domains

--- src/test/java/seedu/triplog/model/trip/EmailTest.java
+++ src/test/java/seedu/triplog/model/trip/EmailTest.java
@@ -50,7 +50,6 @@ public class EmailTest {
         assertFalse(Email.isValidEmail("peterjack@example.com.")); // domain name ends with a period
         assertFalse(Email.isValidEmail("peterjack@-example.com")); // domain name starts with a hyphen
         assertFalse(Email.isValidEmail("peterjack@example.com-")); // domain name ends with a hyphen
-        assertFalse(Email.isValidEmail("peterjack@example.c")); // top level domain has less than two chars
 
         // valid email
         assertTrue(Email.isValidEmail("PeterJack_1190@example.com")); // underscore in local part
@@ -64,6 +63,7 @@ public class EmailTest {
         assertTrue(Email.isValidEmail("peter_jack@very-very-very-long-example.com")); // long domain name
         assertTrue(Email.isValidEmail("if.you.dream.it_you.can.do.it@example.com")); // long local part
         assertTrue(Email.isValidEmail("e1234567@u.nus.edu")); // more than one period in domain
+        assertTrue(Email.isValidEmail("peterjack@example.c")); // top level domain has one char
     }
 
     @Test
